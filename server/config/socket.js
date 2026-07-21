85b657c1eb9753c76d693b1026f97fafe1a871bd
Abhishek6540 <abhishek9872615721@gmail.com>
2026-07-21 20:04:41 +0530

    change initial socket with cors

--- server/config/socket.js
+++ server/config/socket.js
@@ -3,24 +3,11 @@ import chatSocketController from "../sockets/chatSocket.js";
 
 let io;
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:3000",
-  "http://127.0.0.1:3000",
-].filter(Boolean);
-
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-          return;
-        }
-
-        callback(new Error("Not allowed by CORS"));
-      },
-      credentials: true,
+      origin: "*",
+      methods: ["GET", "POST"],
     },
     transports: ["websocket", "polling"],
   });
